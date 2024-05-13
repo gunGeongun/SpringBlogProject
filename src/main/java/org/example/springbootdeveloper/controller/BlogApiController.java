ec19e02bc42ed4519a33cf5111b6a9830eea6aae
geon <kky8586696@naver.com>
2024-05-13 23:58:09 +0900

    글 수정 구현

--- src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.dto.AddArticleRequest;
 import org.example.springbootdeveloper.dto.ArticleResponse;
+import org.example.springbootdeveloper.dto.UpdateArticleRequest;
 import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +46,13 @@ public class BlogApiController {
     public ResponseEntity<Void> deleteArticle(@PathVariable long id){
         blogService.delete(id);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request){
+        Article updatedArticle =blogService.update(id,request);
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
