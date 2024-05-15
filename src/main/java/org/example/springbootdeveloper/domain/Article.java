70ebcf720dd92265c4cddf1ae0eb8812c39fa4ab
geon <kky8586696@naver.com>
2024-05-15 15:19:05 +0900

    타임리프, CRUD 기능 추가

--- src/main/java/org/example/springbootdeveloper/domain/Article.java
+++ src/main/java/org/example/springbootdeveloper/domain/Article.java
@@ -5,31 +5,47 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
-    @Column(name = "id",updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name= "title",nullable = false)
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content",nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
     @Builder
-    public Article(String title,String content){
+    public Article(String title, String content) {
         this.title = title;
         this.content = content;
     }
-    public void update(String title,String content){
-        this.title =title;
+
+    public void update(String title, String content) {
+        this.title = title;
         this.content = content;
     }
-
-
 }
